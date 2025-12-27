982656107049a7478728edc17f83f9485c34e3d2
Alan Tse <alandtse@gmail.com>
2025-12-27 11:30:23 -0800

    chore: remove unused tests

--- tests/shaders/test_lightingcommon.cpp
+++
@@ -1,6 +0,0 @@
-// Tests for Common/LightingCommon.hlsli
-#include "test_helpers_unified.h"
-
-// Shininess to Roughness
-SHADER_TEST_SIMPLE("LightingCommon - Shininess to Roughness", "[lightingcommon][conversion]",
-	"/Shaders/Tests/TestLightingCommon.hlsl", "TestShininessToRoughness")
