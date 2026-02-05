5365d46e0d3f4211c92753b5ed1def1a59223b0e
Alan Tse <alandtse@gmail.com>
2026-02-05 00:14:24 -0800

    fix(VR): apply per eye upscaling

--- src/Utils/UI.h
+++ src/Utils/UI.h
@@ -16,11 +16,22 @@ struct ImVec2;
 class Menu;
 class Feature;
 
-#define BUFFER_VIEWER_NODE(a_value, a_scale)                                                                 \
-	if (ImGui::TreeNode(#a_value)) {                                                                         \
-		ImGui::Image(a_value->srv.get(), { a_value->desc.Width * a_scale, a_value->desc.Height * a_scale }); \
-		ImGui::TreePop();                                                                                    \
+// Helper macro for displaying texture buffers in ImGui with resolution info
+#define BUFFER_VIEWER_NODE_IMPL(a_value, a_label, a_scale)                                                       \
+	if (a_value && a_value->srv.get()) {                                                                         \
+		char buf[128];                                                                                           \
+		snprintf(buf, sizeof(buf), "%s (%ux%u)", a_label, a_value->desc.Width, a_value->desc.Height);            \
+		if (ImGui::TreeNode(buf)) {                                                                              \
+			ImGui::Image(a_value->srv.get(), { a_value->desc.Width * a_scale, a_value->desc.Height * a_scale }); \
+			ImGui::TreePop();                                                                                    \
+		}                                                                                                        \
 	}
+
+#define BUFFER_VIEWER_NODE(a_value, a_scale) \
+	BUFFER_VIEWER_NODE_IMPL(a_value, #a_value, a_scale)
+
+#define BUFFER_VIEWER_NODE_TITLE(a_value, a_title, a_scale) \
+	BUFFER_VIEWER_NODE_IMPL(a_value, a_title, a_scale)
 
 #define BUFFER_VIEWER_NODE_BULLET(a_value, a_scale) \
 	ImGui::BulletText(#a_value);                    \
