eb9de5874d3c249492a51294f59cf160ed9a8c42
ParticleTroned <248299730+ParticleTroned@users.noreply.github.com>
2026-03-01 11:32:58 +0000

    chores: Update Feature version.h and Version check text in features.cpp

--- include/FeatureVersions.h
+++ include/FeatureVersions.h
@@ -14,33 +14,33 @@ namespace FeatureVersions
         {"DynamicCubemaps"sv,      {2,2,2}},
         {"ExtendedMaterials"sv,    {1,1,0}},
         {"ExtendedTranslucency"sv, {1,0,0}},
-        {"GrassCollision"sv,       {3,0,2}}, // ←  CS 1.4.7 requires
+        {"GrassCollision"sv,       {3,0,2}}, 
         {"GrassLighting"sv,        {2,0,0}},
-        {"HairSpecular"sv,         {1,0,3}},  // ←  CS 1.4.7 requires
+        {"HairSpecular"sv,         {1,0,3}},  
         {"ImageBasedLighting"sv,   {1,0,1}},
         {"InteriorSun"sv,          {1,0,0}},
         {"InverseSquareLighting"sv,{1,1,0}},
         {"LODBlending"sv,          {1,0,0}},
-        {"LightLimitFix"sv,        {3,0,0}},
-        {"LinearLighting"sv,       {1,0,0}},  // ←  CS 1.4.7
+        {"LightLimitFix"sv,        {3,1,0}},
+        {"LinearLighting"sv,       {1,0,0}},  
         {"PerformanceOverlay"sv,   {1,0,0}},
         {"RenderDoc"sv,            {1,0,0}},
-        {"ScreenSpaceGI"sv,        {4,0,1}},  // CS 1.4.7 
+        {"ScreenSpaceGI"sv,        {4,0,1}},  
         {"ScreenSpaceShadows"sv,   {2,0,0}}, 
         {"SkySync"sv,              {1,0,0}},
-        {"Skylighting"sv,          {1,2,3}}, // CS 1.4.7
-        {"SubsurfaceScattering"sv, {3,0,1}},  //CS 1.4.7
+        {"Skylighting"sv,          {1,2,3}}, 
+        {"SubsurfaceScattering"sv, {3,0,1}},  
         {"TerrainBlending"sv,      {1,0,1}},
         {"TerrainHelper"sv,        {1,0,0}},
         {"TerrainShadows"sv,       {1,0,0}},
         {"TerrainVariation"sv,     {1,0,1}},
         {"UnifiedWater"sv,         {1,0,0}},
-        {"Upscaling"sv,            {1,2,0}},  // ←  CS 1.4.11
+        {"Upscaling"sv,            {1,2,0}},  
         {"VR"sv,                   {1,0,1}},
         {"VolumetricLighting"sv,   {1,0,0}},
         {"WaterEffects"sv,         {1,0,1}},
         {"WeatherPicker"sv,        {1,0,0}},
-        {"WeatherEditor"sv,        {1,0,0}},  // ←  CS 1.4.7
+        {"WeatherEditor"sv,        {1,0,0}},  
         {"WetnessEffects"sv,       {3,0,0}},
     };
 }
