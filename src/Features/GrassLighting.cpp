591e5a70f8cffc869540c3e28406b1cdfa5387a1
BigBullmoose <65634364+BigBullmoose@users.noreply.github.com>
2026-07-07 23:03:28 -0700

    Add wrapped lighting toggle to GrassLighting settings
    Added a checkbox for enabling wrapped lighting with a tooltip explaining its functionality.

--- src/Features/GrassLighting.cpp
+++ src/Features/GrassLighting.cpp
@@ -11,6 +11,7 @@ NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE_WITH_DEFAULT(
 	SubsurfaceScatteringAmount,
 	OverrideComplexGrassSettings,
 	BasicGrassBrightness,
+	EnableWrappedLighting,
 	ComplexGrassThreshold)
 
 void GrassLighting::DrawSettings()
@@ -56,6 +57,12 @@ void GrassLighting::DrawSettings()
 	}
 
 	if (ImGui::TreeNodeEx(T(TKEY("lighting"), "Lighting"), ImGuiTreeNodeFlags_DefaultOpen)) {
+				ImGui::Checkbox("Enable Wrapped Lighting", (bool*)&settings.EnableWrappedLighting);
+		if (auto _tt = Util::HoverTooltipWrapper()) {
+			ImGui::Text("Enables a softer-looking wrapped lighting model from CS 1.3. Useful for certain non-complex grass textures that look too dark during mid day, when the sun is directly overhead.");
+		}
+		ImGui::Spacing();
+		ImGui::Spacing();
 		ImGui::Checkbox(T(TKEY("override_complex"), "Override Complex Grass Lighting Settings"), (bool*)&settings.OverrideComplexGrassSettings);
 		if (auto _tt = Util::HoverTooltipWrapper()) {
 			ImGui::Text("%s", T(TKEY("override_complex_tooltip"),
